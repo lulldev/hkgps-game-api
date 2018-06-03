3b19c31e9bb12da14d78db1cbaa4a3c3d8044fb2
lulldev <lulldev@yandex.ru>
2018-06-03 14:23:59 +0300

    add create-game route

--- index.route.js
+++ index.route.js
@@ -1,23 +1,7 @@
 const express = require('express');
-const userRoutes = require('./server/user/user.route');
-const authRoutes = require('./server/auth/auth.route');
-const gameRoutes = require('./server/game/game.route');
+const createGameRoute = require('./server/game/create-game.route');
 
 const router = express.Router(); // eslint-disable-line new-cap
-
-// TODO: use glob to match *.route files
-
-/** GET /health-check - Check service health */
-router.get('/health-check', (req, res) =>
-  res.send('OK')
-);
-
-// mount user routes at /users
-router.use('/users', userRoutes);
-
-// mount auth routes at /auth
-router.use('/auth', authRoutes);
-
-router.use('/game', gameRoutes);
+router.use('/create-game', createGameRoute);
 
 module.exports = router;
--- server/game/game.controller.js
+++ server/game/game.controller.js
@@ -1,24 +1,4 @@
-const User = require('./game.model');
-
-/**
- * Load user and append to req.
- */
-function load(req, res, next, id) {
-  // User.get(id)
-  //   .then((user) => {
-  //     req.user = user; // eslint-disable-line no-param-reassign
-  //     return next();
-  //   })
-  //   .catch(e => next(e));
-}
-
-/**
- * Get user
- * @returns {User}
- */
-function get(req, res) {
-  return res.json(req.user);
-}
+const Game = require('./game.model');
 
 /**
  * Create new user
@@ -26,17 +6,38 @@ function get(req, res) {
  * @property {string} req.body.mobileNumber - The mobileNumber of user.
  * @returns {User}
  */
-function create(req, res, next) {
-  return res.json({ test: 'configure' });
-  const user = new User({
-    username: req.body.username,
-    mobileNumber: req.body.mobileNumber
+function createGame(req, res, next) {
+  // console.log(req.query);
+  const game = new Game({
+    disclosure_position_timeout: req.query.disclosure_position_timeout,
+    victim_count: req.query.victim_count,
+    predator_id: req.query.predator_id,
+    status: 'stop'
   });
 
-  // user.save()
-  //   .then(savedUser => res.json(savedUser))
-  //   .catch(e => next(e));
+  game.save().then(savedUser => res.json(savedUser)).catch(e => next(e));
 }
+
+/**
+ * Load user and append to req.
+ */
+// function load(req, res, next, id) {
+//   // User.get(id)
+//   //   .then((user) => {
+//   //     req.user = user; // eslint-disable-line no-param-reassign
+//   //     return next();
+//   //   })
+//   //   .catch(e => next(e));
+// }
+
+/**
+ * Get user
+ * @returns {User}
+ */
+// function get(req, res) {
+//   return res.json(req.user);
+// }
+
 
 /**
  * Update existing user
@@ -44,15 +45,15 @@ function create(req, res, next) {
  * @property {string} req.body.mobileNumber - The mobileNumber of user.
  * @returns {User}
  */
-function update(req, res, next) {
-  const user = req.user;
-  user.username = req.body.username;
-  user.mobileNumber = req.body.mobileNumber;
-
-  user.save()
-    .then(savedUser => res.json(savedUser))
-    .catch(e => next(e));
-}
+// function update(req, res, next) {
+//   const user = req.user;
+//   user.username = req.body.username;
+//   user.mobileNumber = req.body.mobileNumber;
+//
+//   user.save()
+//     .then(savedUser => res.json(savedUser))
+//     .catch(e => next(e));
+// }
 
 /**
  * Get user list.
@@ -60,23 +61,23 @@ function update(req, res, next) {
  * @property {number} req.query.limit - Limit number of users to be returned.
  * @returns {User[]}
  */
-function list(req, res, next) {
-  return { test: 'configure' };
-  const { limit = 50, skip = 0 } = req.query;
-  User.list({ limit, skip })
-    .then(users => res.json(users))
-    .catch(e => next(e));
-}
+// function list(req, res, next) {
+//   return { test: 'configure' };
+//   const { limit = 50, skip = 0 } = req.query;
+//   User.list({ limit, skip })
+//     .then(users => res.json(users))
+//     .catch(e => next(e));
+// }
 
 /**
  * Delete user.
  * @returns {User}
  */
-function remove(req, res, next) {
-  const user = req.user;
-  user.remove()
-    .then(deletedUser => res.json(deletedUser))
-    .catch(e => next(e));
-}
+// function remove(req, res, next) {
+//   const user = req.user;
+//   user.remove()
+//     .then(deletedUser => res.json(deletedUser))
+//     .catch(e => next(e));
+// }
 
-module.exports = { load, get, create, update, list, remove };
+module.exports = { createGame };
